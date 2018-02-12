f37ccdabad9f865187e9c09d9665ffcc0f8ed2cb
刘忍 <liuren6@gmail.com>
2018-02-12 14:06:39 +0800

    添加自动化更新脚本
    sh update.sh 即可

---
+++ update.sh
@@ -0,0 +1,11 @@
+# 自动部署脚本
+
+# 编译网站
+jekyll build
+mv _site/archives.html _site/archives/index.html
+
+# 上传网站到服务器
+scp -r /Users/liuren/Desktop/An0nym6.github.io/_site ubuntu@ranthot.cn:/www/
+
+# 在服务端执行更新
+ssh ubuntu@ranthot.cn "sudo nginx -s reload"
